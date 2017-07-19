98372ba6a5c8bae9fe0f69f51477c12480c6f8af
demente <olga.agen@gmail.com>
2017-07-19 23:07:13 +0300

    Model draft

--- src/my/superfood/model/Food.java
+++ src/my/superfood/model/Food.java
@@ -4,10 +4,6 @@ public class Food {
 
 	String name;
 	Integer weightPerServing;
-	// TODO: shall we extract nutritional info into separate class? And have it
-	// as per 100 grams?
-	Long protein;
-	Long fat;
-	Long carbohydrates;
+	NutritionalInformation nutritionPerHundredGrams;
 
 }
