9c72259c1997a172e6db0646ea9c8d1095b6d869
olga.agen <olga.agen@kuehne-nagel.com>
2018-06-22 16:15:48 +0200

    Add all entities to entity builder factory

--- src/test/java/my/superfood/model/EntityFactory.java
+++ src/test/java/my/superfood/model/EntityFactory.java
@@ -1,6 +1,5 @@
 package my.superfood.model;
 
-import my.superfood.model.Food;
 import net.karneim.pojobuilder.GeneratePojoBuilder;
 
 public class EntityFactory {
@@ -13,4 +12,48 @@ public class EntityFactory {
         return new Food();
     }
 
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static FoodInMealPlan newFoodInMealPlan() {
+        return new FoodInMealPlan();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static Ingredient newIngredient() {
+        return new Ingredient();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static MealPlan newMealPlan() {
+        return new MealPlan();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static MineralAmount newMineralAmount() {
+        return new MineralAmount();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static NutritionalInformation newNutritionalInformation() {
+        return new NutritionalInformation();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static Recipe newRecipe() {
+        return new Recipe();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static RecipeInMealPlan newRecipeInMealPlan() {
+        return new RecipeInMealPlan();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static VitaminAmount newVitaminAmount() {
+        return new VitaminAmount();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static Weight newWeight() {
+        return new Weight();
+    }
 }
