60c5c0dec91e3eb53de44c3eb14de882d3c42597
olga.agen <olga.agen@kuehne-nagel.com>
2018-06-22 16:12:59 +0200

    Add all dtos to builder factory

--- src/test/java/my/superfood/dto/DtoFactory.java
+++ src/test/java/my/superfood/dto/DtoFactory.java
@@ -1,6 +1,5 @@
 package my.superfood.dto;
 
-import my.superfood.dto.FoodDto;
 import net.karneim.pojobuilder.GeneratePojoBuilder;
 
 public class DtoFactory {
@@ -9,8 +8,37 @@ public class DtoFactory {
     }
 
     @GeneratePojoBuilder(withGenerationGap = true)
+    public static IngredientDto newIngredientDto() {
+        return new IngredientDto();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static MineralDto newMineralDto() {
+        return new MineralDto();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static NutritionalInformationDto newNutritionalInformationDto() {
+        return new NutritionalInformationDto();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static RecipeDto newRecipeDto() {
+        return new RecipeDto();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static VitaminDto newVitaminDto() {
+        return new VitaminDto();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
+    public static WeightDto newWeightDto() {
+        return new WeightDto();
+    }
+
+    @GeneratePojoBuilder(withGenerationGap = true)
     public static FoodDto newFoodDto() {
         return new FoodDto();
     }
-
 }
