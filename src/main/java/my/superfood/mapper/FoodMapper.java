1c4742397599dfa14e593e80bbd2159fdd058c96
olga.agen <olga.agen@kuehne-nagel.com>
2018-06-22 16:05:19 +0200

    Adapt FoodMapper to new entity structure

--- src/main/java/my/superfood/mapper/FoodMapper.java
+++ src/main/java/my/superfood/mapper/FoodMapper.java
@@ -1,7 +1,6 @@
 package my.superfood.mapper;
 
 import my.superfood.dto.FoodDto;
-import my.superfood.dto.FoodShortRepresentation;
 import my.superfood.model.Food;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
@@ -10,36 +9,18 @@ import org.mapstruct.factory.Mappers;
 
 import java.util.List;
 
-@Mapper(uses = {MineralMapper.class, VitaminMapper.class, NutritionalInformationMapper.class})
+@Mapper(uses = {NutritionalInformationMapper.class, WeightMapper.class})
 public interface FoodMapper {
 
     FoodMapper INSTANCE = Mappers.getMapper(FoodMapper.class);
 
-    @Mappings({@Mapping(source = "nutritionPerHundredGrams.protein", target = "protein"),
-            @Mapping(source = "nutritionPerHundredGrams.fat", target = "fat"),
-            @Mapping(source = "nutritionPerHundredGrams.carbohydrates", target = "carbohydrates"),
-            @Mapping(source = "nutritionPerHundredGrams.fiber", target = "fiber"),
-            @Mapping(source = "nutritionPerHundredGrams.calories", target = "calories"),
-            @Mapping(source = "nutritionPerHundredGrams.vitamins", target = "vitamins"),
-            @Mapping(source = "nutritionPerHundredGrams.minerals", target = "minerals")})
-    FoodDto toFoodRepresentation(Food food);
+    @Mappings({@Mapping(source = "nutritionPerHundredGrams", target = "nutritionalInformation")})
+    FoodDto toFoodDto(Food food);
 
-    FoodShortRepresentation toFoodShortRepresentation(Food food);
-
-    @Mappings({@Mapping(source = "protein", target = "nutritionPerHundredGrams.protein"),
-            @Mapping(source = "fat", target = "nutritionPerHundredGrams.fat"),
-            @Mapping(source = "carbohydrates", target = "nutritionPerHundredGrams.carbohydrates"),
-            @Mapping(source = "fiber", target = "nutritionPerHundredGrams.fiber"),
-            @Mapping(source = "calories", target = "nutritionPerHundredGrams.calories"),
-            @Mapping(source = "vitamins", target = "nutritionPerHundredGrams.vitamins"),
-            @Mapping(source = "minerals", target = "nutritionPerHundredGrams.minerals")})
+    @Mappings({@Mapping(source = "nutritionalInformation", target = "nutritionPerHundredGrams")})
     Food toFood(FoodDto food);
 
-    Food toFood(FoodShortRepresentation food);
-
-    List<FoodDto> toFoodRepresentationList(List<Food> food);
+    List<FoodDto> toFoodDtoList(List<Food> food);
 
     List<Food> toFoodList(List<FoodDto> foodDto);
-
-    List<FoodShortRepresentation> toFoodShortRepresentationList(List<Food> food);
 }
---
+++ src/test/java/my/superfood/mapper/FoodMapperTest.java
@@ -0,0 +1,85 @@
+package my.superfood.mapper;
+
+import my.superfood.dto.FoodDto;
+import my.superfood.model.Food;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static my.superfood.dto.FoodDtoBuilder.aFoodDto;
+import static my.superfood.model.FoodBuilder.aFood;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.BDDMockito.then;
+
+@RunWith(MockitoJUnitRunner.class)
+public class FoodMapperTest {
+
+    @InjectMocks
+    private FoodMapper foodMapper = new FoodMapperImpl();
+
+    @Mock
+    private NutritionalInformationMapper nutritionalInformationMapper;
+    @Mock
+    private WeightMapper weightMapper;
+
+    @Test
+    public void mapsDtoToEntity() {
+        FoodDto expected = aFoodDto().build();
+
+        Food actual = foodMapper.toFood(expected);
+
+        assertThat(actual.getId()).isEqualTo(expected.getId());
+        assertThat(actual.getType().name()).isEqualTo(expected.getType());
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+    }
+
+    @Test
+    public void mapsEntityToDto() {
+        Food expected = aFood().build();
+
+        FoodDto actual = foodMapper.toFoodDto(expected);
+
+        assertThat(actual.getId()).isEqualTo(expected.getId());
+        assertThat(actual.getType()).isEqualTo(expected.getType().name());
+        assertThat(actual.getName()).isEqualTo(expected.getName());
+    }
+
+    @Test
+    public void mapsToWeightDto() {
+        Food food = aFood().build();
+
+        foodMapper.toFoodDto(food);
+
+        then(weightMapper).should().toWeightDto(food.getWeightPerServing());
+    }
+
+    @Test
+    public void mapsToNutritionalInformationDto() {
+        Food food = aFood().build();
+
+        foodMapper.toFoodDto(food);
+
+        then(nutritionalInformationMapper).should().toNutritionalInformationDto(food.getNutritionPerHundredGrams());
+    }
+
+    @Test
+    public void mapsToWeight() {
+        FoodDto food = aFoodDto().build();
+
+        foodMapper.toFood(food);
+
+        then(weightMapper).should().toWeight(food.getWeightPerServing());
+    }
+
+    @Test
+    public void mapsToNutritionalInformation() {
+        FoodDto food = aFoodDto().build();
+
+        foodMapper.toFood(food);
+
+        then(nutritionalInformationMapper).should().toNutritionalInformation(food.getNutritionalInformation());
+    }
+
+}
