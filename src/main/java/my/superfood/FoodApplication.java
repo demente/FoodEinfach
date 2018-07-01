afe9e911a0aa3e9c3d70d88e961e3a7469abb544
demente <olga.agen@gmail.com>
2018-07-01 17:42:50 +0200

    Add FoodInfoResource

--- src/main/java/my/superfood/FoodApplication.java
+++ src/main/java/my/superfood/FoodApplication.java
@@ -8,13 +8,10 @@ import io.dropwizard.setup.Environment;
 import my.superfood.dao.FoodDao;
 import my.superfood.dao.RecipeDao;
 import my.superfood.healthchecks.DatabaseConnectionHealthCheck;
-import my.superfood.model.Food;
-import my.superfood.model.Ingredient;
-import my.superfood.model.MineralAmount;
-import my.superfood.model.Recipe;
+import my.superfood.model.*;
+import my.superfood.resources.FoodInfoResource;
 import my.superfood.resources.FoodResource;
 import my.superfood.resources.RecipeResource;
-import my.superfood.model.VitaminAmount;
 import org.eclipse.jetty.servlets.CrossOriginFilter;
 
 import javax.servlet.DispatcherType;
@@ -24,7 +21,6 @@ import java.util.EnumSet;
 public class FoodApplication extends Application<FoodConfiguration> {
 
     private final HibernateBundle<FoodConfiguration> hibernateBundle = new HibernateBundle<FoodConfiguration>(
-            Food.class, MineralAmount.class, VitaminAmount.class, Ingredient.class, Recipe.class) {
         public DataSourceFactory getDataSourceFactory(FoodConfiguration configuration) {
             return configuration.getDataSourceFactory();
         }
@@ -47,6 +43,7 @@ public class FoodApplication extends Application<FoodConfiguration> {
         final FoodDao foodDao = new FoodDao(hibernateBundle.getSessionFactory());
         RecipeDao recipeDao = new RecipeDao(hibernateBundle.getSessionFactory());
         environment.jersey().register(new FoodResource(foodDao));
+        environment.jersey().register(new FoodInfoResource(foodDao));
         environment.jersey().register(new RecipeResource(recipeDao));
 
         environment.healthChecks().register("database", new DatabaseConnectionHealthCheck(null));
