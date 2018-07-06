17016554a73bb39a451666bfc777e859e7ce2301
demente <olga.agen@gmail.com>
2018-07-06 15:09:28 +0200

    add dao test

--- src/test/java/my/superfood/dao/FoodDaoTest.java
+++ src/test/java/my/superfood/dao/FoodDaoTest.java
@@ -1,5 +1,42 @@
-import static org.junit.Assert.*;
+package my.superfood.dao;
+
+import io.dropwizard.testing.junit.DAOTestRule;
+import my.superfood.model.Food;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import static my.superfood.model.FoodBuilder.aFood;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class FoodDaoTest {
 
+    @Rule
+    public DAOTestRule database = DAOTestRule.newBuilder().addEntityClass(Food.class).build();
+    private FoodDao foodDao;
+
+    @Before
+    public void setUp() {
+        foodDao = new FoodDao(database.getSessionFactory());
+    }
+
+    @Test
+    public void createsFood() {
+        Food food = database.inTransaction(() -> {
+            return foodDao.save(aFood().build());
+        });
+
+        assertThat(food.getId()).isNotNull();
+    }
+
+    @Test
+    public void roundtripsFoo() {
+        Food food = database.inTransaction(() -> {
+            return foodDao.save(aFood().build());
+        });
+
+        Food f = foodDao.findById(food.getId());
+
+        assertThat(f).isSameAs(food);
+    }
 }
