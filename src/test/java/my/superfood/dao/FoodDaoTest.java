7019cad274e4750a90850244df02d19bbc08649c
demente <olga.agen@gmail.com>
2018-07-06 21:45:35 +0200

    improve food dao test

--- src/test/java/my/superfood/dao/FoodDaoTest.java
+++ src/test/java/my/superfood/dao/FoodDaoTest.java
@@ -1,18 +1,28 @@
 package my.superfood.dao;
 
 import io.dropwizard.testing.junit.DAOTestRule;
-import my.superfood.model.Food;
+import my.superfood.model.*;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.List;
+
 import static my.superfood.model.FoodBuilder.aFood;
+import static my.superfood.model.FoodBuilder.aNewFood;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class FoodDaoTest {
 
     @Rule
-    public DAOTestRule database = DAOTestRule.newBuilder().addEntityClass(Food.class).build();
+    public DAOTestRule database = DAOTestRule.newBuilder()
+            .addEntityClass(Food.class)
+            .addEntityClass(VitaminAmount.class)
+            .addEntityClass(Vitamin.class)
+            .addEntityClass(MineralAmount.class)
+            .addEntityClass(Mineral.class)
+            .build();
+
     private FoodDao foodDao;
 
     @Before
@@ -21,22 +31,48 @@ public class FoodDaoTest {
     }
 
     @Test
-    public void createsFood() {
-        Food food = database.inTransaction(() -> {
-            return foodDao.save(aFood().build());
-        });
+    public void savesFood() {
+        Food food = database.inTransaction(() -> foodDao.save(aNewFood().build()));
 
         assertThat(food.getId()).isNotNull();
     }
 
     @Test
-    public void roundtripsFoo() {
-        Food food = database.inTransaction(() -> {
-            return foodDao.save(aFood().build());
-        });
+    public void findsById() {
+        Food expected = database.inTransaction(() -> foodDao.save(aNewFood().build()));
 
-        Food f = foodDao.findById(food.getId());
+        Food actual = foodDao.findById(expected.getId());
 
-        assertThat(f).isSameAs(food);
+        assertThat(actual).isSameAs(expected);
+    }
+
+    @Test
+    public void deletesFood() {
+        Food persisted = database.inTransaction(() -> foodDao.save(aNewFood().build()));
+
+        foodDao.delete(persisted.getId());
+
+        assertThat(foodDao.findById(persisted.getId())).isNull();
+    }
+
+    @Test
+    public void findsAll() {
+        Food apple = database.inTransaction(() -> foodDao.save(aNewFood().withName("Apple").build()));
+        Food orange = database.inTransaction(() -> foodDao.save(aNewFood().withName("Orange").build()));
+
+        List<Food> actual = foodDao.findAll();
+
+        assertThat(actual).containsExactly(apple, orange);
+    }
+
+    @Test
+    public void findsByName() {
+        Food apple = database.inTransaction(() -> foodDao.save(aNewFood().withName("Apple").build()));
+        Food apricot = database.inTransaction(() -> foodDao.save(aNewFood().withName("Apricot").build()));
+        database.inTransaction(() -> foodDao.save(aNewFood().withName("Orange").build()));
+
+        List<Food> actual = foodDao.findByName("AP");
+
+        assertThat(actual).containsExactly(apple, apricot);
     }
 }
