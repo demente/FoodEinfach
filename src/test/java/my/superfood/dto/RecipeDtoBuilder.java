69f937c4fc0faf8702442a48df6de6b7c97de00a
demente <olga.agen@gmail.com>
2018-07-07 11:34:47 +0200

    Add default values to builder

--- src/test/java/my/superfood/dto/RecipeDtoBuilder.java
+++ src/test/java/my/superfood/dto/RecipeDtoBuilder.java
@@ -2,6 +2,9 @@ package my.superfood.dto;
 
 import javax.annotation.Generated;
 
+import static java.util.Arrays.asList;
+import static my.superfood.dto.IngredientDtoBuilder.anIngredientDto;
+
 @Generated("PojoBuilder")
 public class RecipeDtoBuilder extends AbstractRecipeDtoBuilder {
 
@@ -9,6 +12,13 @@ public class RecipeDtoBuilder extends AbstractRecipeDtoBuilder {
     }
 
     public static RecipeDtoBuilder aRecipeDto() {
-        return new RecipeDtoBuilder();
+        return new RecipeDtoBuilder()
+                .withId(1L)
+                .withName("Baked apples")
+                .withServings(2L)
+                .withType(asList("BREAKFAST"))
+                .withInstructions("Cut the apples and put them in the oven.")
+                .withPreparationTime(10L)
+                .withIngredients(asList(anIngredientDto().build()));
     }
 }
