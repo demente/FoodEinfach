a6a2f481268cf02c53acc49e8d12fac2f9bff846
demente <olga.agen@gmail.com>
2018-07-07 13:36:43 +0200

    Add mapping from mineral to mineral dto

--- src/main/java/my/superfood/mapper/MineralMapper.java
+++ src/main/java/my/superfood/mapper/MineralMapper.java
@@ -1,11 +1,15 @@
 package my.superfood.mapper;
 
 import my.superfood.dto.MineralAmountDto;
+import my.superfood.dto.MineralDto;
+import my.superfood.model.Mineral;
 import my.superfood.model.MineralAmount;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.Mappings;
 import org.mapstruct.factory.Mappers;
+
+import java.util.List;
 
 @Mapper(uses = {WeightMapper.class})
 public interface MineralMapper {
@@ -18,4 +22,8 @@ public interface MineralMapper {
     @Mappings({@Mapping(target = "name", source = "mineral.name"),
             @Mapping(target = "dailyNorm", source = "mineral.dailyNorm")})
     MineralAmountDto toMineralAmountDto(MineralAmount mineralAmount);
+
+    MineralDto toMineralDto(Mineral mineral);
+
+    List<MineralDto> toMineralDtoList(List<Mineral> mineralList);
 }
--- src/test/java/my/superfood/mapper/MineralMapperTest.java
+++ src/test/java/my/superfood/mapper/MineralMapperTest.java
@@ -1,6 +1,8 @@
 package my.superfood.mapper;
 
 import my.superfood.dto.MineralAmountDto;
+import my.superfood.dto.MineralDto;
+import my.superfood.model.Mineral;
 import my.superfood.model.MineralAmount;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -9,6 +11,7 @@ import org.mockito.junit.MockitoJUnitRunner;
 
 import static my.superfood.dto.MineralAmountDtoBuilder.aMineralAmountDto;
 import static my.superfood.model.MineralAmountBuilder.aMineralAmount;
+import static my.superfood.model.MineralBuilder.aMineral;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(MockitoJUnitRunner.class)
@@ -18,7 +21,7 @@ public class MineralMapperTest {
     private MineralMapper mineralMapper = new MineralMapperImpl();
 
     @Test
-    public void mapsEntityToDto() {
+    public void mapsMineralAmountToDto() {
         MineralAmount expected = aMineralAmount().build();
 
         MineralAmountDto actual = mineralMapper.toMineralAmountDto(expected);
@@ -28,12 +31,22 @@ public class MineralMapperTest {
     }
 
     @Test
-    public void mapsDtoToEntity() {
+    public void mapsMineralAmountDtoToEntity() {
         MineralAmountDto expected = aMineralAmountDto().build();
 
         MineralAmount actual = mineralMapper.toMineralAmount(expected);
 
         assertThat(expected.getId()).isEqualTo(actual.getId());
         assertThat(expected.getName()).isEqualTo(actual.getMineral().getName());
+    }
+
+    @Test
+    public void mapsMineralToDto() {
+        Mineral expected = aMineral().build();
+
+        MineralDto actual = mineralMapper.toMineralDto(expected);
+
+        assertThat(actual.getName()).isEqualTo(expected.getName().name());
+        assertThat(actual.getDailyNorm().getWeight()).isEqualTo(expected.getDailyNorm());
     }
 }
